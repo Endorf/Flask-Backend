8fc92c64fd8a7e2e2a4db348a206ee107c99050a
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-06-10 19:21:59 +0300

    add tests

---
+++ app/tests/test_app.py
@@ -0,0 +1,24 @@
+import pytest
+from app.views import app
+from app.service.common import status
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_health(client):
+    """ It should be healthy """
+    response = client.get('/health')
+    assert response.status_code == status.HTTP_200_OK
+
+
+if __name__ == '__main__':
+    pytest.main()
