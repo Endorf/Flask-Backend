3d47ed65c28c7c95b544986c069c57c9b335c577
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-06-10 19:49:47 +0300

    fix SC1091

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -5,6 +5,7 @@ cd /var/www/flask-app || exit
 yum install python3-pip -y
 
 python3 -m venv .venv
+# shellcheck source=/dev/null
 source /var/www/flask-app/.venv/bin/activate
 
 pip3 install --upgrade pip
