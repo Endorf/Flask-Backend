7ef84c1cd9738601924362f909ad4289174e8969
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-06-30 16:54:10 +0300

    Update config

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
 from app import views
+from app import routes
--- config.py
+++ config.py
@@ -2,11 +2,20 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SESSION_PERMANENT = False
+    DATABASE = 'test_db'
 
 
 class ProductionConfig(Config):
-    pass
+    """
+    Production configurations
+    """
+    DATABASE = 'prod_db'
 
 
 class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+    DATABASE = 'dev_db'
     DEBUG = True
+    TESTING = True
