98e30e9c517437111d20f1ea9c841c54543e7133
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-27 19:24:31 +0300

    update config

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ class Config(object):
     TESTING = False
     SESSION_PERMANENT = False
     DATABASE = 'test_db'
+    OAUTH2_CLIENT_ID = "test_web_app"
+    OAUTH2_CLIENT_SECRET = 'BbyZrpYjSf6JRxOEs1tVBFUcYVcfAYIQ'
+    OAUTH2_ISSUER = 'http://localhost:8080/realms/myorg'
+    OAUTH2_ISSUER_HOST = "http://localhost:8080"
+    FLASK_SECRET = 'somelongrandomstring'
+    FLASK_PORT: 5000
 
 
 class ProductionConfig(Config):
