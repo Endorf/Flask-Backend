73d76a0a30fd7fdde32d09270e5b4a679f829d8b
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-29 17:33:34 +0300

    update auth service

--- app/auth/service.py
+++ app/auth/service.py
@@ -1,24 +1,104 @@
 from app import app
-from flask import session
 import requests
 
 
-def authenticateUser(email, password):
-    post_body = f"client_id={ app.config['OAUTH2_CLIENT_ID'] }&client_secret={ app.config['OAUTH2_CLIENT_SECRET'] }&grant_type=password&scope=email roles profile&username={email.split('@')[0]}&password={password}"
-    headers = {'Content-Type': "application/x-www-form-urlencoded"}
+def refresh(token):
+    post_body = f"client_id={ app.config['OAUTH2_CLIENT_ID'] }&client_secret={ app.config['OAUTH2_CLIENT_SECRET'] }&grant_type=refresh_token&refresh_token={ token }"
+    headers = {'Content-Type': "application/x-www-form-urlencoded",}
     url = app.config['OAUTH2_ISSUER'] + "/protocol/openid-connect/token"
 
-    accessTokenResp = requests.post(
+    response = requests.post(
         url,
         data=post_body,
         headers=headers
     )
-    accessTokenRespJson = accessTokenResp.json()
+    return response
 
-    if "access_token" in accessTokenRespJson:
-        session["user"] = accessTokenRespJson
-        session["email"] = email
-        session["access_token"] = accessTokenRespJson["access_token"]
-        session["refresh_token"] = accessTokenRespJson["refresh_token"]
 
-    return accessTokenResp
+def logout(refresh_token, authorization_header):
+    post_body = f"client_id={ app.config['OAUTH2_CLIENT_ID'] }&client_secret={ app.config['OAUTH2_CLIENT_SECRET'] }&refresh_token={ refresh_token }"
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Authorization': authorization_header,
+    }
+    url = app.config['OAUTH2_ISSUER'] + "/protocol/openid-connect/logout"
+
+    response = requests.post(
+        url,
+        data=post_body,
+        headers=headers
+    )
+    return response
+
+
+def authenticate(email, password):
+    post_body = f"client_id={ app.config['OAUTH2_CLIENT_ID'] }&client_secret={ app.config['OAUTH2_CLIENT_SECRET'] }&grant_type=password&scope=email roles profile&username={email.split('@')[0]}&password={password}"
+    headers = {'Content-Type': "application/x-www-form-urlencoded"}
+    url = app.config['OAUTH2_ISSUER'] + "/protocol/openid-connect/token"
+
+    response = requests.post(
+        url,
+        data=post_body,
+        headers=headers
+    )
+
+    return response
+
+
+def register(email, password):
+    accessToken = _retrieveAdminAccessToken()
+    # if(accessToken == ""):
+        # return False  # TODO: return error
+
+    post_body = {
+        "username": email.split("@")[0],
+        "email": email,
+        "enabled": True,
+        "credentials": [{
+            "type": "password",
+            "value": password,
+            "temporary": False
+        }],
+        "groups": []
+    }
+    headers = {
+        'Authorization': "Bearer " + accessToken,
+        'Content-Type': "application/json; charset=utf-8",
+    }
+    url = app.config['OAUTH2_ISSUER_HOST'] + "/admin/realms/myorg/users"
+
+    response = requests.post(
+        url,
+        json=post_body,
+        headers=headers
+    )
+
+    if (response.ok):
+        return authenticate(email, password)
+    else:    
+        return response
+
+
+def _retrieveAdminAccessToken():
+    post_body = {
+        "grant_type": "client_credentials",
+        "client_id": app.config['OAUTH2_CLIENT_ID'],
+        "client_secret": app.config['OAUTH2_CLIENT_SECRET'],
+        "scope": ["test_api_access"]
+    }
+    headers = {'Content-Type': "application/x-www-form-urlencoded"}
+    url = app.config['OAUTH2_ISSUER'] + "/protocol/openid-connect/token"
+
+    response = requests.post(
+        url,
+        data=post_body,
+        headers=headers
+    )
+    
+    if not response.ok:
+        return ""
+    else:
+        responseJson = response.json()
+        if not "access_token" in responseJson:
+            return ""
+        return responseJson["access_token"]
