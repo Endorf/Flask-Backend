c474d55a47a3c0c42292e8102d20109ee228bf27
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-29 17:32:58 +0300

    create client api

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,41 +1,69 @@
 from app import app
 
-from urllib.request import urlopen
-from flask import jsonify, request, session
+from flask import request, session
+from authlib.integrations.flask_oauth2 import ResourceProtector
 
-from .service import authenticateUser
-
-
-@app.route("/api/auth", methods=["GET", "POST"])
-def auth_client():
-    user = request.args.get('user')
-    password = request.args.get('password')
-
-    print(f"{user} : {password}")
-
-    data = authenticateUser(email=user, password=password)
-    print(f"{data} ")
-
-    response = session['user']
-    
-    return jsonify(message=response)
+from .service import authenticate, register, logout, refresh
+from app.auth.token_validator import ClientTokenValidator
 
 
+require_auth = ResourceProtector()
+validator = ClientTokenValidator("http://localhost:8080/realms/myorg")
+require_auth.register_token_validator(validator)
+
+ 
 @app.route("/auth/signup", methods=["POST"])
 def signup_client():
-    pass
+
+    email = request.json['email']
+    password = request.json['password']
+
+    response = register(email, password)
+
+    return (
+        response.json(), 
+        response.status_code, 
+        {'ContentType':'application/json'}
+    )
 
 
 @app.route("/auth/signin", methods=["POST"])
 def signin_client():
-    pass
+    email = request.json['email']
+    password = request.json['password']
+
+    response = authenticate(email, password)
+    
+    return (
+        response.json(), 
+        response.status_code, 
+        {'ContentType':'application/json'}
+    )
 
 
 @app.route("/auth/refresh", methods=["POST"])
 def refresh_client():
-    pass
+    token = request.json['token']
+
+    response = refresh(token)
+
+    return (
+        response.json(), 
+        response.status_code, 
+        {'ContentType':'application/json'}
+    )
 
 
 @app.route("/auth/logout", methods=["POST"])
+@require_auth(None)
 def logout_client():
-    pass
+    refresh_token = request.json['refresh_token']
+
+    response = logout(refresh_token, request.headers['Authorization'])
+    session.clear()
+
+    return (
+        "", 
+        response.status_code, 
+        {'ContentType':'application/json'}
+    )
